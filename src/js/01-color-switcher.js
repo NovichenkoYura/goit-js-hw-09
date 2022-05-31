9bc62e2b698b4a8146edec58ae1d67107371a02f
NovichenkoYura <novichenko.y@gmail.com>
2022-05-31 17:25:40 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const onBtnStart = document.querySelector('button[data-start]');
+const onBtnStop = document.querySelector('button[data-stop]');
+const bodyBgc = document.querySelector('body')
+
+onBtnStart.addEventListener('click', onClickBtnStart);
+onBtnStop.addEventListener('click', onClickBtnStop)
+
+let timerId = null;
+
+function changeColor() {
+    timerId = setInterval(() => {
+        bodyBgc.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+
+}
+
+function onClickBtnStart() {
+    onBtnStart.disabled = true;
+    changeColor()
+
+}
+
+function onClickBtnStop() {
+    clearInterval(timerId);
+    onBtnStart.disabled = false;
+}
