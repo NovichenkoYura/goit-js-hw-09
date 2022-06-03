5bb22c33be2e757ecca64dafcac92ad6e737b0d1
NovichenkoYura <novichenko.y@gmail.com>
2022-06-03 10:32:40 +0300

    task2 - bad(

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -38,7 +38,6 @@ function clockView(dateOffset) {
     secondQty.textContent = dateOffset.seconds;
 }
 
-flatpickr(inputDate, options)
 
 const options = {
   enableTime: true,
@@ -46,9 +45,12 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    selectDate(selectedDates[0]);
   },
 };
+
+
+flatpickr(inputDate, options)
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -69,6 +71,23 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+
+function selectDate(selectTime){
+    if (!onBtnStart.disabled) {
+        return;
+    } 
+    else{
+    timeFinish = selectTime.getTime()
+    delta = timeFinish-timeStart;
+    
+    if (delta <= 0) {
+    window.alert("Please choose a date in the future");
+      return;
+    }
+    else {
+    onBtnStart.disabled = false;
+    return timeFinish;
+    }
+  }
+};
