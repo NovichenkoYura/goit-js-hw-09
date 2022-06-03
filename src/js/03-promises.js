683589ad5c20c0f530b40b1b3c667915cfc8b482
NovichenkoYura <novichenko.y@gmail.com>
2022-06-03 16:15:28 +0300

    clarify task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,60 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector(".form");
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  // if (shouldResolve) {
+  //   // Fulfill
+  // } else {
+  //   // Reject
+  // }
+
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (shouldResolve) {
+      return resolve({ position, delay })    
+    }
+    else {
+      return reject({ position, delay })
+    }
+      
+  }, delay)
+
+})
+  
+  promise
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+  
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
 }
+
+// Notiflix.Notify.success('Sol lucet omnibus');
+
+// Notiflix.Notify.failure('Qui timide rogat docet negare');
+
+
+const onFormSubmit = e => {
+  e.preventDefault();
+ 
+  const firstDelay = Number(e.currentTarget[0].value);
+  const delayStep = Number(e.currentTarget[1].value);
+  const amount = Number(e.currentTarget[2].value)
+
+  let delay = firstDelay;
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+
+    delay += delayStep
+  }
+
+}
+
+form.addEventListener('submit', onFormSubmit) 
